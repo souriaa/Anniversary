b7d7094d00ca9187d69cd20b3ca205b829a42ffb
souria <132901245+souriaa@users.noreply.github.com>
2024-05-22 01:55:31 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,39 @@
+const CONFIG = {
+    titleWeb: "Happy Anniversary",
+    introTitle: 'Sữa ơiii',
+    introDesc: `Em có biết hôm nay là ngày gì khummm😗`,
+    btnIntro: 'Bíc!!!',
+    title: 'Happy 2 months Anniversary, Sữa iuu của anh',
+    desc: `Chúng ta cùng nghe bài nhạc làm ta nhớ đến nhau trước nheeee.
+    Sau đó, em có thể ấn "Sữa đã nghe xong" ở dưới nhaaaa.`,
+    btnYes: 'Sữa đã nghe xonggggg',
+    btnNo: 'Chưa nghe xong😠',
+    question: 'Còn 1 điều nữaaaaa nèee',
+    btnReply: 'Ấn để xemm',
+    reply: '',
+    mess: `Thư Anniversary`,
+    messDesc: `
+    <div style="text-align: left" >
+        Gửi Sữa yêu dấu❤️:<br><br>
+
+        Helluu Sữa bé iuuuu.<br><br>
+
+        Anh viết thư này là để kỉ niệm 2 tháng chúng ta bắt đầu yêu. Vì anh ở xa nên không viết thư tay cho em được. Vì vậy nên anh quyết định làm một chiếc web cuti này dành riêng cho em.<br><br>
+
+        Anh sẽ không nói lại những điều anh đã viết trong lá thư trước vì những điều chân thật đó sẽ không bao giờ thay đổi. Em luôn là người đáng yêu, xinh đẹp trong mắt anh, một người luôn quan tâm cho anh. Mặc dù dạo gần đây, anh biết em đang áp lực và mệt mỏi với những môn học trên trường, những sự kiện phải chạy, những dự án phải care và cả việc dạy học của em, nên em luôn phải thức khuya, luôn phải đâm đầu vào những deadline để rồi không có nhiều thời gian nhắn tin cho nhau như lúc mới yêu.<br><br>
+
+        Mặc dù có thể em không thể hiện hết ra bên ngoài khi ở cạnh anh nhưng anh luôn cảm nhận được điều đó! Nhưng em à, anh sẽ luôn là người cố gắng cùng em, luôn là người ở bên cạnh em mỗi khi bão tố ùa đến và luôn là người giúp em xua tan đi những cảm xúc tiêu cực tồn đọng trong em! Và anh luôn tin những sự cố gắng và sự xa cách nhau sẽ làm chúng ta hạnh phúc hơn.<br><br>
+
+        Cuối cùng, anh chỉ muốn nói rằng, em hãy tiếp tục cố gắng và nỗ lực vượt qua khoảng thời gian khó khăn này nhé. Anh sẽ luôn là người đồng hành cùng em, vượt qua những ngày tồi tệ để cùng nhau tìm thấy hạnh phúc. Nhưng em cũng đừng quên giữ gìn sức khỏe, ăn đủ bữa đúng giờ, uống nước đầy đủ và cố gắng đừng thức khuya dậy sớm em nhéee.<br><br>
+
+        Anh mong chúng ta sẽ luôn hạnh phúc, kể cả khi chúng ta có cách xa nhau.<br><br>
+
+        I'll love you until the end of time<br><br>
+
+        <span style="text-align: right; display: block; margin-right: 2vh">Your boyfriend</span>
+</div>
+`,
+
+    btnAccept: 'Nút này là để đóng thư',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
